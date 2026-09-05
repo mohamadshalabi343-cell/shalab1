208cd66444edd627c6f748078a1df019ed7f2f2e
mohamadshalabi343-cell <mohamadshalabi343@gmail.com>
2026-09-05 22:58:29 +0300

    Update database.py

--- database.py
+++ database.py
@@ -5,19 +5,19 @@ db = SQLAlchemy()
 
 class Worker(db.Model):
     __tablename__ = 'workers'
-
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
+    
     repairs = db.relationship('RepairRecord', backref='worker', lazy=True)
-
+    
     def __repr__(self):
         return f'<Worker {self.name}>'
 
 class RepairRecord(db.Model):
     __tablename__ = 'repair_records'
-
+    
     id = db.Column(db.Integer, primary_key=True)
     worker_id = db.Column(db.Integer, db.ForeignKey('workers.id'), nullable=False)
     device_name = db.Column(db.String(200), nullable=False)
@@ -29,17 +29,27 @@ class RepairRecord(db.Model):
     status = db.Column(db.String(50), default='تم التسليم')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
+    
     @property
     def profit(self):
-        return self.amount_received - self.cost
-
+        """الربح لا يحسب إلا إذا كانت الحالة 'تم التسليم'"""
+        if self.status == 'تم التسليم':
+            return self.amount_received - self.cost
+        return 0
+    
+    @property
+    def effective_amount_received(self):
+        """المبلغ المقبوض يُعتد به فقط إذا كانت الحالة 'تم التسليم'"""
+        if self.status == 'تم التسليم':
+            return self.amount_received
+        return 0
+    
     @property
     def profit_margin(self):
-        if self.amount_received > 0:
+        if self.amount_received > 0 and self.status == 'تم التسليم':
             return (self.profit / self.amount_received) * 100
         return 0
-
+    
     def to_dict(self):
         return {
             'id': self.id,
@@ -48,6 +58,7 @@ class RepairRecord(db.Model):
             'device_model': self.device_model or '',
             'cost': self.cost,
             'amount_received': self.amount_received,
+            'effective_amount_received': self.effective_amount_received,
             'profit': self.profit,
             'profit_margin': round(self.profit_margin, 1),
             'issues': self.issues or '',
@@ -56,6 +67,6 @@ class RepairRecord(db.Model):
             'created_at': self.created_at.strftime('%Y-%m-%d %H:%M') if self.created_at else '',
             'updated_at': self.updated_at.strftime('%Y-%m-%d %H:%M') if self.updated_at else ''
         }
-
+    
     def __repr__(self):
         return f'<RepairRecord {self.device_name} - {self.worker.name}>'
